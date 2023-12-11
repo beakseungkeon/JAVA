38413ed2e181fa4e2f5bfe74d6c9a67181f402f8
beakseungkeon <eric7829@naver.com>
2023-12-11 14:40:09 +0900

    홀짝 판별 테스트 12-11

---
+++ Java_bsg/src/day02/TestEx2.java
@@ -0,0 +1,20 @@
+package day02;
+
+public class TestEx2 {
+	//홀짝 판별 예제
+	public static void main(String[] args) {
+		int num=3;
+		if(num==0) {
+			System.out.println("0입니다.");
+		}
+		else if(num%2==1) {
+			System.out.println("홀수입니다.");
+		}
+		else {
+			System.out.println("짝수입니다.");
+		}
+		
+
+	}
+
+}
