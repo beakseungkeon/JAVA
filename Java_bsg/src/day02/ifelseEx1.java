38c1ace170112c95e6076b87561abaab03452acb
beakseungkeon <eric7829@naver.com>
2023-12-11 14:19:46 +0900

    12/11 ifelse문

---
+++ Java_bsg/src/day02/ifelseEx1.java
@@ -0,0 +1,16 @@
+package day02;
+
+public class ifelseEx1 {
+
+	public static void main(String[] args) {
+		
+		int num1=10;
+		if(num1==0) {
+			System.out.println("0임");;
+		}
+		else {
+			System.out.println(num1+"은 0이 아님");
+		}
+	}
+
+}
