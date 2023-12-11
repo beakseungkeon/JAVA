16d77d0a37c68eee0903c320fc1888d40920e395
beakseungkeon <eric7829@naver.com>
2023-12-11 14:39:54 +0900

    if else 문 12-11

--- Java_bsg/src/day02/ifelseEx1.java
+++ Java_bsg/src/day02/ifelseEx1.java
@@ -8,6 +8,8 @@ public class ifelseEx1 {
 		if(num1==0) {
 			System.out.println("0임");;
 		}
+		//if를 두번쓰면 무조건 2번 확인
+		//if else를 쓰면 num가 0이면 1번확인,num가 0이 아니면 2번 확인
 		else {
 			System.out.println(num1+"은 0이 아님");
 		}
