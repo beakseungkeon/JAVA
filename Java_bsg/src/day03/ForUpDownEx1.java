67435f27ad059a8454efd3086665d0bc66f62506
beakseungkeon <eric7829@naver.com>
2023-12-12 15:43:16 +0900

    for문 업다운 12-12

---
+++ Java_bsg/src/day03/ForUpDownEx1.java
@@ -0,0 +1,29 @@
+package day03;
+
+import java.util.Scanner;
+
+public class ForUpDownEx1 {
+
+	public static void main(String[] args) {
+		int min=1,max=100;
+		int r=(int)(Math.random()*(max-min+1)+1);
+		System.out.println("정수를 입력하세요");
+		Scanner scan = new Scanner(System.in);
+		
+		
+		int num=0;
+		for( ; num!=r; ) {
+			
+			num=scan.nextInt();
+			System.out.println("정수:"+num);
+			if(num>r) {
+				System.out.println("DOWN");
+			}
+			else System.out.println("UP");
+		}
+		System.out.println("정답");
+		scan.close();
+
+	}
+
+}
