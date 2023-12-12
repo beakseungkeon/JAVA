fabc83cfcb628b89ccb6d250a331b1c426e06bd1
beakseungkeon <eric7829@naver.com>
2023-12-12 11:36:17 +0900

    1~5 while 문 출력 12-12

---
+++ Java_bsg/src/day03/WhileTestEx1.java
@@ -0,0 +1,30 @@
+package day03;
+
+public class WhileTestEx1 {
+
+	public static void main(String[] args) {
+
+/* 1부터 5까지 출력되는 코드를 작성하세요*/
+		
+		
+		int i=1;
+		while(i<=5) {
+			System.out.println(i);
+			i++;
+		}
+		
+		
+		i=1;
+		System.out.println(i);
+		i++;
+		System.out.println(i);
+		i++;
+		System.out.println(i);
+		i++;
+		System.out.println(i);
+		i++;
+		System.out.println(i);
+		
+	}
+
+}
