5f0b915a2a78ea4e2d6b130f295ae46c098687d6
beakseungkeon <eric7829@naver.com>
2023-12-12 17:01:53 +0900

    알파벳 출력

---
+++ Java_bsg/src/alphabetEx1.java
@@ -0,0 +1,21 @@
+
+public class alphabetEx1 {
+
+	public static void main(String[] args) {
+		//a부터 z까지 반복문을 이용하여 출력하는 코드
+		//65부터
+		char alpha='a';
+		while(true) {
+			System.out.println(alpha);
+			if(alpha=='z') {
+				
+				break;
+			}
+			
+			
+			alpha++;
+		}
+		
+	}
+
+}
