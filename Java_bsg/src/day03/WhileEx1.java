5ab9b00250eba99e4d699ff2f178fdf147d09586
beakseungkeon <eric7829@naver.com>
2023-12-12 11:36:01 +0900

    while문 12-12

---
+++ Java_bsg/src/day03/WhileEx1.java
@@ -0,0 +1,15 @@
+package day03;
+
+public class WhileEx1 {
+	//while문 예제
+	public static void main(String[] args) {
+	//Hello World를 5번 출력하는 예제
+		System.out.println("hello_world");
+		int i=0;//초기화
+		while(i<5) {
+			System.out.println("hello_world");//실행문
+			i++;//증감식
+		}
+	}
+
+}
