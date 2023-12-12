3cf532cd1db30738a52b0debc7bae53ec7314ead
beakseungkeon <eric7829@naver.com>
2023-12-12 15:42:32 +0900

    for문 반복 출력 12-12

---
+++ Java_bsg/src/day03/ForEx1.java
@@ -0,0 +1,13 @@
+package day03;
+
+public class ForEx1 {
+
+	public static void main(String[] args) {
+		/*Hello World 5번 출력 예제*/
+		for(int i=0;i<5;i++) {
+			System.out.println("HelloWorld");
+		}
+
+	}
+
+}
