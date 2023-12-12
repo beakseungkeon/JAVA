72c675394ace4e59fc1b325de9419618fcfbd807
beakseungkeon <eric7829@naver.com>
2023-12-12 17:02:51 +0900

    Update LoopEx1.java

--- Java_bsg/src/day03/LoopEx1.java
+++ Java_bsg/src/day03/LoopEx1.java
@@ -7,7 +7,7 @@ public class LoopEx1 {
 	public static void main(String[] args) {
 		//문자를 입력받아 입력받은 문자를 출력하고,q를 입력하면 종료하는 예제를
 		Scanner scan = new Scanner(System.in);
-		scan.close();
+		
 		char ch='a';
 		for( ;ch!='q'; ) {
 			System.out.println("입력:");
