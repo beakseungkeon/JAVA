73a21cffee954e42159fdeb9d9a8721602a9d7d3
beakseungkeon <eric7829@naver.com>
2023-12-12 12:10:51 +0900

    5~1 줄어드는 출력 12-12

---
+++ Java_bsg/src/day03/WhileTestEx2.java
@@ -0,0 +1,16 @@
+package day03;
+
+public class WhileTestEx2 {
+
+	public static void main(String[] args) {
+		/*5부터 1까지 줄어드는 코드*/
+		int i=5;
+		
+		while(i>=1) {
+			System.out.println(i);
+			i--;
+		}
+
+	}
+
+}
