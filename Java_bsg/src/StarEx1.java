151f9bf91051e9330c01d3fb179f14af111c3d65
beakseungkeon <eric7829@naver.com>
2023-12-13 12:13:34 +0900

    별 오른쪽으로 늘어나기 12-13

---
+++ Java_bsg/src/StarEx1.java
@@ -0,0 +1,14 @@
+
+public class StarEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0;i<3;i++) {
+			for(int a=1;a<=5;a++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
