01087785c9e660f7707b707f4ce9ee807a6dbacb
beakseungkeon <eric7829@naver.com>
2023-12-13 17:25:54 +0900

    무한루프2

---
+++ Java_bsg/src/day04/InfiniteLoopEx2.java
@@ -0,0 +1,18 @@
+package day04;
+
+import java.util.Scanner;
+
+public class InfiniteLoopEx2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		int num;
+		for(int i=10;i>0;) {
+			System.out.println("정수 입력:");
+			 num=scan.nextInt();
+			System.out.println("정수 출력: "+num);
+		}
+	}
+
+}
