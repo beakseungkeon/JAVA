f8743c71bde6dcf3c5b04dda75c235b5a81acbcb
beakseungkeon <eric7829@naver.com>
2023-12-13 12:33:21 +0900

    별 12-13

---
+++ Java_bsg/src/StarEx4.java
@@ -0,0 +1,21 @@
+
+public class StarEx4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int b=1;
+		int d=0;
+		for(int i=0;i<5;i++) {
+			for(int c=4;c>=d;c--) {
+				System.out.print(" ");
+			}
+			for(int a=1;a<=b;a++) {
+				System.out.print("*");
+			}
+			d+=1;
+			b+=2;
+			System.out.println();
+		}
+	}
+
+}
