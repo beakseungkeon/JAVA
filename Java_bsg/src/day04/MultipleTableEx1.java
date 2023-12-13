e3b2c4f05102f1ee426e66876d2268b0000a04f7
beakseungkeon <eric7829@naver.com>
2023-12-13 12:12:39 +0900

    구구단 12-13

---
+++ Java_bsg/src/day04/MultipleTableEx1.java
@@ -0,0 +1,16 @@
+package day04;
+
+public class MultipleTableEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//구구단 2단부터 9단까지 출력하는 코드 작성
+		
+		for(int i=2;i<=9;i++) {
+			for(int a=1;a<=9;a++) {
+				System.out.println(i+"x"+a+"="+i*a);
+			}
+		}
+	}
+
+}
