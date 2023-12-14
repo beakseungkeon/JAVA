1dbe4d77897216b4388a417e66ba264416552314
beakseungkeon <eric7829@naver.com>
2023-12-14 17:17:23 +0900

    Update InfiniteLoopEx2.java

--- Java_bsg/src/day04/InfiniteLoopEx2.java
+++ Java_bsg/src/day04/InfiniteLoopEx2.java
@@ -13,6 +13,16 @@ public class InfiniteLoopEx2 {
 			 num=scan.nextInt();
 			System.out.println("정수 출력: "+num);
 		}
+		//조건식에 따라 True가 될수도 있기 떄문에 에러 발생 x
+		System.out.println("프로그램 종료");
+		
+		//위 코드는 무한루프이고 아래 코드는 무수히 많이 실행
+		//int의 범위를 벗어나면 오버플로우 현상에 의해 음수로 바뀌면서 멈춤
+		for(int i=10;i>0;) {
+			System.out.println("정수 입력:");
+			 num=scan.nextInt();
+			System.out.println("정수 출력: "+num);
+		}
 	}
 
 }
