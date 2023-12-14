7069e121754f969a69e0a26492fd6ef0629a23e7
beakseungkeon <eric7829@naver.com>
2023-12-14 17:16:50 +0900

    배열 예제1

---
+++ Java_bsg/src/day05/ArrayEx1.java
@@ -0,0 +1,36 @@
+package day05;
+
+import java.util.Scanner;
+
+public class ArrayEx1 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		/*int kor1,kor2,kor3,kor4,kor5;
+		kor1=scan.nextInt();
+		kor2=scan.nextInt();
+		kor3=scan.nextInt();
+		kor4=scan.nextInt();
+		kor5=scan.nextInt();
+		
+		
+		//학생 5명의 국어 성적을 저장하기 위한 배열
+		korScores[0]=scan.nextInt();
+		korScores[1]=scan.nextInt();
+		korScores[2]=scan.nextInt();
+		korScores[3]=scan.nextInt();
+		korScores[4]=scan.nextInt();*/
+		
+		//반복문 사용
+		int[]korScores=new int[5];
+		for(int i=0;i<=4;i++) {
+			System.out.print("학생"+(i+1)+"성적:");
+			korScores[i]=scan.nextInt();
+		}
+		for(int i=0;i<=4;i++) {
+			System.out.println("학생"+(i+1)+"성적:"+korScores[i]);
+		}
+		scan.close();
+	}
+
+}
