998b7464c28ef5e0c523adaed3b81c76315eae31
beakseungkeon <eric7829@naver.com>
2023-12-14 17:16:43 +0900

    배열 예제2

---
+++ Java_bsg/src/day05/ArrayEx2.java
@@ -0,0 +1,15 @@
+package day05;
+
+public class ArrayEx2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int []arr1;
+		
+		int arr2[];
+		
+		int []arr3,arr4;
+		int arr5[],arr6;
+	}
+
+}
