36e40d567dc3a1bf734faf230025b20dfa9f16b8
beakseungkeon <eric7829@naver.com>
2023-12-15 12:38:12 +0900

    산술연산12-15

---
+++ Java_bsg/src/day05/ArithmeticEx1.java
@@ -0,0 +1,41 @@
+package day05;
+
+import java.util.Scanner;
+
+public class ArithmeticEx1 {
+
+	public static void main(String[] args) {
+		// 랜덤으로 산수(+,-,X)문제를 생성하여 맞추는 게임
+		int answer=0;
+		Scanner scan = new Scanner(System.in);
+		char san[]=new char[] {'+','-','x'};
+		int min=0,max=2;
+		int Min=1,Max=99;
+		while(true){
+		int num=(int)(Math.random()*(Max-Min+1)+Min);
+		int num2=(int)(Math.random()*(Max-Min+1)+Min);
+		int am=(int)(Math.random()*(max-min+1)+min);
+		
+		System.out.print(num);
+		System.out.print(san[am]);
+		System.out.print(num2+"=");
+		int input=scan.nextInt();
+		
+		switch(san[am]) {
+		case '+':answer=num+num2;break;
+		case '-':answer=num-num2;break;
+		case 'x':answer=num*num2;break;
+			}
+		if(input==answer) {
+			System.out.println("정답입니다.");
+		}
+		else {
+			System.out.println("틀렸습니다");
+			System.out.println("정답은:"+answer);
+		}
+			
+		}
+		
+	}
+
+}
