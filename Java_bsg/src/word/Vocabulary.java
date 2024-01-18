ae97f3ad30e4ae179cddcbac979f5e4ccf675d00
beakseungkeon <110386782+beakseungkeon@users.noreply.github.com>
2024-01-19 04:38:51 +0900

    123

---
+++ Java_bsg/src/word/Vocabulary.java
@@ -0,0 +1,22 @@
+package word;
+
+import java.util.ArrayList;
+import java.util.List;
+//단어장
+public class Vocabulary {
+	private List<Word> list;//단어 리스트
+
+	public Vocabulary(List<Word> list) {
+		if(list==null) {
+			list=new ArrayList<Word>();
+		}
+		this.list = list;
+	}
+	//단어 추가 기능
+	public boolean addWord(String word,String partOfSpeech,String mean) {
+		if
+		//이미 등록된 단어이고,뜻도 등록되었으면
+		
+		return true;
+	}
+}
